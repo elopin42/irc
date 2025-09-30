a14ec5d05ca5f7eb7f568c721e0ad2c9e2a70e70
Yann Le Jaouen <yle-jaou@f2r5s11.clusters.42paris.fr>
2025-09-30 18:29:16 +0200

    2-3 changements dans les structs

--- incl/struct.hpp
+++ incl/struct.hpp
@@ -1,32 +1,37 @@
 #ifndef STRUCT_HPP
 # define STRUCT_HPP
 
-#include <iostream>
-#include <string>
+# include <iostream>
+# include <string>
 
-struct rules {
-  int port;
-  std::string password;
+typedef struct rules
+{
+	int	port;
+	std::string password;
+}		t_rules;
+
+typedef struct server
+{
+}		t_server;
+
+class client
+{
+  public:
+	client();
+	~client();
+
+  private:
+	std::string password;
+	std::string nickname;
+	std::string username;
 };
 
-class client {
-public:
-  client();
-  ~client();
-
-private:
-  std::string password;
-  std::string nickname;
-  std::string username;
-};
-
-client::client() {
+client::client()
+{
 }
 
-client::~client() {
+client::~client()
+{
 }
-
 
 #endif // !STRUCT_HPP
-
-
